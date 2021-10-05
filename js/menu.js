be3a7bd7ec4f526cd84724352e5d383173266d03
Moa2405 <62264443+Moa2405@users.noreply.github.com>
2021-10-05 21:10:19 +0200

    dev 12

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,24 @@
 
-const burgerMenu = document.querySelector("div.burger i")
-const nav = document.querySelector("nav")
-const arrowBack = document.querySelector("div.arrow")
+// const burgerMenu = document.querySelector("div.burger i")
+// const nav = document.querySelector("nav")
+// const arrowBack = document.querySelector("div.arrow")
 
-burgerMenu.addEventListener("click", function (event) {
-    console.log("in")
-    burgerMenu.style.display = "none"
-    arrowBack.style.display = "block"
-    nav.style.transform = "translate(0%)"
+// burgerMenu.addEventListener("click", function (event) {
+//     console.log("in")
+//     burgerMenu.style.display = "none"
+//     arrowBack.style.display = "block"
+//     nav.style.display = "block"
     
-})
+// })
 
-arrowBack.addEventListener("click", function (event) {
-    console.log('out')
-    burgerMenu.style.display = "block"
-    arrowBack.style.display = "none"
-    nav.style.transform = "translate(110%)"
+// arrowBack.addEventListener("click", function (event) {
+//     console.log('out')
+//     burgerMenu.style.display = "block"
+//     arrowBack.style.display = "none"
+//     nav.style.display = "none"
 
     
-})
+// })
 
 
 
